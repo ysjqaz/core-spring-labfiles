edbec582fa2c4d98736252c1e9dd17921f0221c6
ysj <1241267542@qq.com>
2025-01-03 18:47:20 +0800

    完成java配置实验

--- lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -1,5 +1,16 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import rewards.RewardNetwork;
+import rewards.internal.RewardNetworkImpl;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.account.JdbcAccountRepository;
+import rewards.internal.restaurant.JdbcRestaurantRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.JdbcRewardRepository;
+import rewards.internal.reward.RewardRepository;
+
 import javax.sql.DataSource;
 
 /**
@@ -41,10 +52,40 @@ import javax.sql.DataSource;
  * - Note that return type of each bean method should be an interface
  *   not an implementation.
  */
-
+@Configuration
 public class RewardsConfig {
 
 	// Set this by adding a constructor.
 	private DataSource dataSource;
+
+    public RewardsConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public RewardNetwork rewardNetwork() {
+        return new RewardNetworkImpl(accountRepository(), restaurantRepository(), rewardRepository());
+    }
+
+    @Bean
+    public AccountRepository accountRepository() {
+        JdbcAccountRepository accountRepository = new JdbcAccountRepository();
+        accountRepository.setDataSource(dataSource);
+        return accountRepository;
+    }
+
+    @Bean
+    public RestaurantRepository restaurantRepository() {
+        JdbcRestaurantRepository restaurantRepository = new JdbcRestaurantRepository();
+        restaurantRepository.setDataSource(dataSource);
+        return restaurantRepository;
+    }
+
+    @Bean
+    public RewardRepository rewardRepository() {
+        JdbcRewardRepository rewardRepository = new JdbcRewardRepository();
+        rewardRepository.setDataSource(dataSource);
+        return rewardRepository;
+    }
 
 }
