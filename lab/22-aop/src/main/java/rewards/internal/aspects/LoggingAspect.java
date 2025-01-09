9e81eac1a32c83f1b52579c4d43620cca40efbc2
ysj <1241267542@qq.com>
2025-01-09 19:30:16 +0800

    完成面向切面编程实验

--- lab/22-aop/src/main/java/rewards/internal/aspects/LoggingAspect.java
+++ lab/22-aop/src/main/java/rewards/internal/aspects/LoggingAspect.java
@@ -3,9 +3,13 @@ package rewards.internal.aspects;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.stereotype.Component;
 import rewards.internal.monitor.Monitor;
 import rewards.internal.monitor.MonitorFactory;
 
@@ -16,7 +20,8 @@ import rewards.internal.monitor.MonitorFactory;
 //	- Optionally place @Autowired annotation on the constructor
 //    where `MonitorFactory` dependency is being injected.
 //    (It is optional since there is only a single constructor in the class.)
-
+@Aspect
+@Component
 public class LoggingAspect {
     public final static String BEFORE = "'Before'";
     public final static String AROUND = "'Around'";
@@ -35,7 +40,7 @@ public class LoggingAspect {
 	// - Decide which advice type is most appropriate
 	// - Write a pointcut expression that selects only find* methods on
 	//    our repository classes.
-
+	@Before("execution(* rewards.internal.*.*Repository.find*(..))")
 	public void implLogging(JoinPoint joinPoint) {
 		// Do not modify this log message or the test will fail
 		logger.info(BEFORE + " advice implementation - " + joinPoint.getTarget().getClass() + //
@@ -48,7 +53,7 @@ public class LoggingAspect {
     // - Mark this method as an around advice.
 	// - Write a pointcut expression to match on all update* methods
 	//	 on all Repository classes.
-
+	@Around("execution(* rewards.internal.*.*Repository.update*(..))")
 	public Object monitor(ProceedingJoinPoint repositoryMethod) throws Throwable {
 		String name = createJoinPointTraceName(repositoryMethod);
 		Monitor monitor = monitorFactory.start(name);
@@ -58,8 +63,7 @@ public class LoggingAspect {
 			//  TODO-08: Add the logic to proceed with the target method invocation.
 			//  - Be sure to return the target method's return value to the caller
 			//    and delete the line below.
-
-			return new String("Delete this line after completing TODO-08");
+			return repositoryMethod.proceed();
 
 		} finally {
 			monitor.stop();
